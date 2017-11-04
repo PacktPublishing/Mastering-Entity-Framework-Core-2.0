460cd7d98a1cecfa100f5a285ae76e498c15ce6f
Prabhakaran Anbazhagan <mailme@prabhakarana.com>
2017-11-04 12:33:49 +0530

    Reverting concurrency check added before concurrency chapter

--- Chapter 10/Starter/MasteringEFCore.Concurrencies.Starter/Models/Post.cs
+++ Chapter 10/Starter/MasteringEFCore.Concurrencies.Starter/Models/Post.cs
@@ -25,7 +25,6 @@ namespace MasteringEFCore.Concurrencies.Starter.Models
         public string Url { get; set; }
         public long VisitorCount { get; set; }
         public DateTime CreatedAt { get; set; }
-        [ConcurrencyCheck]
         public DateTime ModifiedAt { get; set; }
         public int CreatedBy { get; set; }
         public int ModifiedBy { get; set; }
