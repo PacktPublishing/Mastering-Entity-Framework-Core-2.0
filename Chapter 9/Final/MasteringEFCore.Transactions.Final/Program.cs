c8915fa1f1487153f6c14dd7ce0f8586ee0820d8
Prabhakaran Anbazhagan <mailme@prabhakarana.com>
2017-12-09 00:09:53 +0530

    Chapter 9 - .net core 2.0 updates

--- Chapter 9/Final/MasteringEFCore.Transactions.Final/Program.cs
+++ Chapter 9/Final/MasteringEFCore.Transactions.Final/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MasteringEFCore.Transactions.Final.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace MasteringEFCore
 {
@@ -11,15 +11,28 @@ namespace MasteringEFCore
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
 
+                try
+                {
+                    var context = services.GetRequiredService<BlogContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred initializing the database.");
+                }
+            }
             host.Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
