d173f18ec0701536a490760c0921cbf7a1cfc5e4
Prabhakaran Anbazhagan <mailme@prabhakarana.com>
2017-12-09 07:46:23 +0530

    Chapter 10 - .NET Core 2.0 update

--- Chapter 10/Final/MasteringEFCore.Concurrencies.Final/Program.cs
+++ Chapter 10/Final/MasteringEFCore.Concurrencies.Final/Program.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MasteringEFCore.Concurrencies.Final.Data;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore;
 
 namespace MasteringEFCore
 {
@@ -11,15 +15,29 @@ namespace MasteringEFCore
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<BlogContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred initializing the database.");
+                }
+            }
 
             host.Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
